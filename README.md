4dc168972f31a0dbbd553a95cc6a85ec268cfe92
pradeep-appsphere <pradeep@appsphere.in>
2021-02-16 21:23:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# scrollable-table
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/scrollable-table)
---
+++ src/app/app.component.ts
@@ -0,0 +1,86 @@
+import { Component, VERSION } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
+})
+export class AppComponent {
+  public items: any[] = [
+    {
+      name: "Pradeep Kumar Sharma",
+      email: "pradeepkrsharma@hotmail.com",
+      designation: "Software Engineer"
+    },
+    {
+      name: "Pradeep Kumar Sharma",
+      email: "pradeepkrsharma@hotmail.com",
+      designation: "Software Engineer"
+    },
+    {
+      name: "Pradeep Kumar Sharma",
+      email: "pradeepkrsharma@hotmail.com",
+      designation: "Software Engineer"
+    },
+    {
+      name: "Pradeep Kumar Sharma",
+      email: "pradeepkrsharma@hotmail.com",
+      designation: "Software Engineer"
+    },
+    {
+      name: "Pradeep Kumar Sharma",
+      email: "pradeepkrsharma@hotmail.com",
+      designation: "Software Engineer"
+    },
+    {
+      name: "Pradeep Kumar Sharma",
+      email: "pradeepkrsharma@hotmail.com",
+      designation: "Software Engineer"
+    },
+    {
+      name: "Pradeep Kumar Sharma",
+      email: "pradeepkrsharma@hotmail.com",
+      designation: "Software Engineer"
+    },
+    {
+      name: "Pradeep Kumar Sharma",
+      email: "pradeepkrsharma@hotmail.com",
+      designation: "Software Engineer"
+    },
+    {
+      name: "Pradeep Kumar Sharma",
+      email: "pradeepkrsharma@hotmail.com",
+      designation: "Software Engineer"
+    },
+    {
+      name: "Pradeep Kumar Sharma",
+      email: "pradeepkrsharma@hotmail.com",
+      designation: "Software Engineer"
+    },
+    {
+      name: "Pradeep Kumar Sharma",
+      email: "pradeepkrsharma@hotmail.com",
+      designation: "Software Engineer"
+    },
+    {
+      name: "Pradeep Kumar Sharma",
+      email: "pradeepkrsharma@hotmail.com",
+      designation: "Software Engineer"
+    },
+    {
+      name: "Pradeep Kumar Sharma",
+      email: "pradeepkrsharma@hotmail.com",
+      designation: "Software Engineer"
+    },
+    {
+      name: "Pradeep Kumar Sharma",
+      email: "pradeepkrsharma@hotmail.com",
+      designation: "Software Engineer"
+    },
+    {
+      name: "Pradeep Kumar Sharma",
+      email: "pradeepkrsharma@hotmail.com",
+      designation: "Software Engineer"
+    }
+  ];
+}
